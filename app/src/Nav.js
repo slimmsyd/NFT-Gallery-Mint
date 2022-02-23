e466140d87f21d84dfdba90015ead611e25f4e4c
Sydney Sanders <ssanderss444@gmail.com>
2022-02-23 18:24:14 -0500

    just unadded web3modal

--- app/src/Nav.js
+++ app/src/Nav.js
@@ -2,7 +2,6 @@ import './App.css';
 import {  Link } from 'react-router-dom';
 import { useEffect, useRef, useState } from 'react';
 import { providers} from 'ethers';
-import Web3Modal from 'web3modal';
 
 
 export default function Nav({accountAddress} ) { 
